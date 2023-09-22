fd122c92537ababbee71e7ccb65e3b4234aa82a2
WR <adm@hamont.com.br>
2023-09-22 16:09:13 -0300

    autenticando usuario

--- upload.js
+++ upload.js
@@ -1,6 +1,8 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getStorage, ref, uploadBytesResumable } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-storage.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+
+
 
 
 const firebaseConfig = {
@@ -12,9 +14,7 @@ const firebaseConfig = {
     appId: "1:875602451810:web:acbaab244dfa8bfecc603b"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const storage = getStorage(app);
 
 const input = document.querySelector('input[type=file]');
@@ -27,64 +27,66 @@ const Belem = document.querySelector('#Belem');
 const Kalimento = document.querySelector('#Kalimento');
 const uploadImage = document.querySelector('#uploadImage');
 const checkbox = document.querySelector('input[type=checkbox]');
-var percent = document.getElementById('percent');
+const percent = document.getElementById('percent');
 
+let files = []; // Array para armazenar as imagens selecionadas pelo usuário
 
-// function logado() {
-//     swal.fire({
-
-//         icon: "success",
-//         title: "Imagem enviada com sucesso!"
-//     });
-// }
-
+input.addEventListener('change', (e) => {
+    files = e.target.files; // Atualiza a variável 'files' com as imagens selecionadas pelo usuário
+});
 
 uploadImage.addEventListener('click', () => {
+    const email = "ismailearliene@gmail.com";
+    const senha = "thomasmanoel@2020";
     let folderName = '';
+
     if (Ceara.checked) {
         folderName = Ceara.name;
     } else if (Ambiental.checked) {
         folderName = Ambiental.name;
     } else if (Balsa.checked) {
         folderName = Balsa.name;
-    }else if (Barracão.checked) {
+    } else if (Barracão.checked) {
         folderName = Barracão.name;
-
-    }else if (Belem.checked) {
+    } else if (Belem.checked) {
         folderName = Belem.name;
-
-    }else if (Kalimento.checked) {
+    } else if (Kalimento.checked) {
         folderName = Kalimento.name;
-
     }
 
-    const files = input.files;
-    for (let i = 0; i < files.length; i++) {
-        let file = files[i];
-        const storageRef = ref(storage, `image/${folderName}/${file.name}`);
-        const uploadTask = uploadBytesResumable(storageRef, file);
+    const user = getAuth(); // Obtenha o objeto de autenticação
 
-        uploadTask.on("state_changed",
-            (snapshot) => {
+    signInWithEmailAndPassword(user, email, senha) // Autentique o usuário
+        .then((userCredential) => {
+            const user = userCredential.user;
 
-                let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-
-                progressBar.value = progress;
-                percent.innerHTML = Math.round((progressBar.value / progressBar.max) * 100) + '%';
-
-            },
-            (error) => {
-                console.log(error);
-            },
-            () => {
-                input.value = ''
-
-                return
-
+            if (!user) {
+                console.error("Usuário não autenticado.");
+                return;
             }
-        );
 
-    }
-})
+            for (let i = 0; i < files.length; i++) {
+                let file = files[i];
+                const storageRef = ref(storage, `image/${folderName}/${file.name}`);
+                const uploadTask = uploadBytesResumable(storageRef, file);
 
-
+                uploadTask.on("state_changed",
+                    (snapshot) => {
+                        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        progressBar.value = progress;
+                        percent.innerHTML = Math.round((progressBar.value / progressBar.max) * 100) + '%';
+                    },
+                    (error) => {
+                        console.log(error);
+                    },
+                    () => {
+                        input.value = '';
+                        console.log("Upload concluído!");
+                    }
+                );
+            }
+        })
+        .catch((error) => {
+            console.error("Erro na autenticação:", error);
+        });
+});
